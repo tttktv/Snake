b4b527e417894006c7b638edae1e760ecc9ca8a3
tttktv <tttktv@gmail.com>
2020-07-30 22:00:58 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public Main(){
+        setTitle("Snake");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        add(new Game());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Main main=new Main();
+    }
+}
